aeba713cb71c1c67464078fc6f5eebb756fc204d
lyysander <geoffrey.espagne@gmail.com>
2013-04-25 00:00:12 +0200

    re-modif history

--- src/game/gui/GameHandler.java
+++ src/game/gui/GameHandler.java
@@ -155,17 +155,18 @@ public class GameHandler implements IGameClient {
 					Games game = new Games(this.model.getPlayersModel());
 					session.persist(game);
 					session.getTransaction().commit();
-//					for (PlayerModel player : game.getGamePlayersInfos().getPlayers()) {
-//						session.beginTransaction();
-//						History history = new History(game.getID(), player.getPlayerID(), player.getPlayerScore());
-//						session.persist(history);
-//						session.getTransaction().commit();
-//					}
-					session.beginTransaction();
-					History history = new History(game.getID(), game.getWinnerID(), game.getWinnerID());
-					session.persist(history);
-					session.getTransaction().commit();
-					session.close();
+					for (PlayerModel player : game.getGamePlayersInfos().getPlayers()) {
+						System.out.println("player : "+player.getPlayerLogin());
+						session.beginTransaction();
+						History history = new History(game.getID(), player.getPlayerID(), player.getPlayerScore());
+						session.persist(history);
+						session.getTransaction().commit();
+					}
+//					session.beginTransaction();
+//					History history = new History(game.getID(), game.getWinnerID(), game.getWinnerID());
+//					session.persist(history);
+//					session.getTransaction().commit();
+//					session.close();
 				}
 				return;
 			}
